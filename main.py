f84eb99a15872d48f6de6c5797414fcb1ca03583
shahzebanwar <shahzebanwer@gmail.com>
2020-07-25 19:57:42 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -18,23 +18,18 @@
  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-import numpy as np
-from random import randint
 import os
 import sys
 import time
 import socket
 import json
 import cv2
+from openvino.inference_engine import IECore, IENetwork
 import logging as log
 import paho.mqtt.client as mqtt
+
 from argparse import ArgumentParser
 from inference import Network
-import time
-    
-INPUT_STREAM = "resources/Pedestrian_Detect_2_1_1.mp4"
-CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
-    
 
 # MQTT server environment variables
 HOSTNAME = socket.gethostname()
@@ -52,10 +47,10 @@ def build_argparser():
     parser = ArgumentParser()
     parser.add_argument("-m", "--model", required=True, type=str,
                         help="Path to an xml file with a trained model.")
-    parser.add_argument("-i", "--input", required=False, type=str,
-                        help="Path to image or video file",default=INPUT_STREAM)
+    parser.add_argument("-i", "--input", required=True, type=str,
+                        help="Path to image or video file")
     parser.add_argument("-l", "--cpu_extension", required=False, type=str,
-                        default = CPU_EXTENSION,
+                        default=None,
                         help="MKLDNN (CPU)-targeted custom layers."
                              "Absolute path to a shared library with the"
                              "kernels impl.")
@@ -74,31 +69,32 @@ def connect_mqtt():
     ### TODO: Connect to the MQTT client ###
     client = mqtt.Client()
     client.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
     return client
 
-def draw_bounding_boxes(frame, result,prob_threshold,width,height):
+def draw_boxes(frame, result, args, width, height):
     '''
     Draw bounding boxes onto the frame.
     '''
-    no_of_person_in_frame = 0
-    PERSON_CLASS = 1
-    for obj in result[0][0]:
-        # Draw bounding box for object when it's probability is more than
-        #  the specified threshold
-        if obj[2] > prob_threshold:
-            xmin = int(obj[3] * width)
-            ymin = int(obj[4] * height)
-            xmax = int(obj[5] * width)
-            ymax = int(obj[6] * height)
-            class_id = int(obj[1])
-            color = (min(class_id * 12.5, 255), min(class_id * 7, 255), min(class_id * 5, 255))
-            if class_id == PERSON_CLASS:
-                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
-                no_of_person_in_frame += 1
-    return frame, no_of_person_in_frame
-               
+    count = 0
+    count_leave=0
+    for box in result[0][0]: # Output shape is 1x1x100x7
+        conf = box[2]
+        if conf >= args.prob_threshold:
+            xmin = int(box[3] * width)
+            ymin = int(box[4] * height)
+            xmax = int(box[5] * width)
+            ymax = int(box[6] * height)
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255,0,0), 2)
+            count = 1
+            if xmin>618 and ymin > 60 and xmax > 764:
+                count_leave = 1
+            
+            
+    return frame, count, count_leave
 
-def infer_on_stream(args,client):
+
+def infer_on_stream(args, client):
     """
     Initialize the inference network, stream video to network,
     and output stats and video.
@@ -106,180 +102,121 @@ def infer_on_stream(args,client):
     :param client: MQTT client
     :return: None
     """
-    frame_count = 0
-    frame_time = 0
-    
-    duration_prev = 0
-    total_count = 0
-    time_thresh = 0    
-    person_count_in_each_frame = 0
-    last_count = 0
-    previous_last_count = 0
-    
-    font_scale = 0.5
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    
-    # Flag for the input image
-    single_image_mode = False
-    
+    counter = 0
+    person = 0
+    detect = 0
+    warning_timer = 0
+    avg_time = 0
+    image_mode = False
     # Initialise the class
-    infer_network = Network()
+    inference_network = Network()
     
     # Set Probability threshold for detections
     prob_threshold = args.prob_threshold
-    model = args.model
-    device = args.device
 
     ### TODO: Load the model through `infer_network` ###
-    infer_network.load_model(model, device, CPU_EXTENSION)
-    infer_network_input_shape = infer_network.get_input_shape()
-    #print("\n.... network input shape...  ",infer_network_input_shape,"\n")
+    n, c, h, w = inference_network.load_model(args.model, args.device, args.cpu_extension)
     
-    # Check if the input is a webcam
-    if args.input == 'CAM':
-        input_Type = 0
-        
-    # Checks for input image
-    elif args.input.endswith('.jpg') or args.input.endswith('.bmp') :
-        single_image_mode = True
-        input_Type = args.input
-
-    # Checks for video file
-    else:
-        input_Type = args.input
-        assert os.path.isfile(args.input), "Specified input file doesn't exist"
-
     ### TODO: Handle the input stream ###
-    input_stream = cv2.VideoCapture(input_Type)
-    if input_Type:
-        input_stream.open(args.input)
-    if not input_stream.isOpened():
-        log.error("ERROR! Unable to open video source")
-    
-    # Grab the shape of the input 
-    width = int(input_stream.get(3))
-    height = int(input_stream.get(4))
-    
-    if not single_image_mode:
-        # The second argument should be `cv2.VideoWriter_fourcc('M','J','P','G')`
-        # on Mac, and `0x00000021` on Linux
-        # 100x100 to match desired resizing
-        out = cv2.VideoWriter('output_video.mp4', 0x00000021, 30, (width,height))
+    if args.input == 'CAM':
+        inputstream = 0 
+    elif args.input.endswith('jpg') or args.input.endswith('bmp') or args.input.endswith('png'):
+        image_mode = True
+        inputstream = args.input
+    elif args.input.endswith('mp4') or args.input.endswith('flv') or args.input.endswith('avi'):
+        inputstream = args.input
     else:
-        out = None
-   
+        print('Input not supported')
+    
+    
+    cap = cv2.VideoCapture(inputstream)
+    cap.open(inputstream)
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
     ### TODO: Loop until stream is over ###
-    while input_stream.isOpened():
+    while cap.isOpened():
+
         ### TODO: Read from the video capture ###
-        flag,frame = input_stream.read()
+        flag, frame = cap.read()
         if not flag:
             break
-        frame_count += 1
-        t = time.time()
         key_pressed = cv2.waitKey(60)
-        
-        ### TODO: Pre-process the image as needed ### n c h w
-        preProcessed_frame = cv2.resize(frame, (infer_network_input_shape[3], infer_network_input_shape[2]))
-        preProcessed_frame = preProcessed_frame.transpose((2,0,1))
-        preProcessed_frame = preProcessed_frame.reshape(1, *preProcessed_frame.shape)
+        inf_start = time.time()
+
+        ### TODO: Pre-process the image as needed ###
+        p_frame = cv2.resize(frame, (w, h))
+        p_frame = p_frame.transpose((2,0,1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
 
         ### TODO: Start asynchronous inference for specified request ###
-        inferencing_start = time.time()
-        total_time_spent = None
-        infer_network.exec_net(preProcessed_frame)
+        inference_network.async_inference(p_frame)
 
         ### TODO: Wait for the result ###
-        if infer_network.wait() == 0:
-            
-            detection_time = time.time() - inferencing_start
+        if inference_network.wait() == 0:
+
+            det_time = time.time() - inf_start
             ### TODO: Get the results of the inference request ###
-            result = infer_network.get_output()
-            frame, current_count = draw_bounding_boxes(frame,result,prob_threshold,width,height)
-            inference_time_message = "Inference time: {:.3f}ms".format(detection_time * 1000)
-            cv2.putText(frame, inference_time_message, (25, 25),cv2.FONT_HERSHEY_COMPLEX, font_scale, (0, 10, 250),1)
-    
+            result = inference_network.extract_output()
+
             ### TODO: Extract any desired stats from the results ###
-            if current_count == last_count:
-                time_thresh += 1
-                if time_thresh >= 10:
-                    person_count_in_each_frame = last_count
-                    if time_thresh == 10 and last_count > previous_last_count:
-                        total_count += last_count - previous_last_count
-                    elif time_thresh == 10 and last_count < previous_last_count:
-                        total_time_spent = int((duration_prev / 10.0) * 1000) # in ms
+            frame, count, count_leave= draw_boxes(frame, result, args, width, height)
+            inf_time_message = "Inference time: {:.3f}ms".format(det_time * 1000)
+            cv2.putText(frame, inf_time_message, (15, 15),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
+    
+            #updating counter to display alarm if person stays longer then 20 sec    
+            warning_timer +=1    
+            
+            #if person is detected in frame
+            if count == 1:
+                detect +=1
+            
+            #if person is detected & did not leave the frame
+            if detect > 0 and count_leave == 0:
+                count = 1
+            
+            #if person leaves the frame
+            elif count_leave == 1:
+                detect = 0
+            
+            #if there is no one  in frame
             else:
-                previous_last_count = last_count
-                last_count = current_count
-                if time_thresh >= 10:
-                    duration_prev = time_thresh
-                    time_thresh = 0
-                else:
-                    time_thresh = duration_prev + time_thresh
-                    
-            current_count_label = "No of Persons : {:.2f}".format(current_count)
-            cv2.putText(frame, current_count_label,(25,50),font, font_scale,(255, 0, 0), 1) 
+                count = 0
             
-            total_count_label = "Total Detected Person : {:.2f}".format(total_count)
-            cv2.putText(frame, total_count_label,(25,75),font, font_scale,(255, 0, 0), 1)
+            if detect == 1 and count_leave == 0:
+                start_time = time.time()
+            if count_leave == 1:
+                person = person + count_leave
+                client.publish("person", json.dumps({"total": person}))
+                duration = int(time.time() - start_time)
+                avg_time = duration
+                client.publish("person/duration", json.dumps({"duration": duration}))
             
-            alert_flag = False
-            alert_msg = None
-            if current_count > 5:
-                alert_msg = "ALERT!!! " + str(current_count) + " persons are at same place"
-                alert_flag = True                                  
-            if total_time_spent is not None and total_time_spent > 3000000: # 5 min
-                alert_msg = "ALERT!!! " + str(current_count) + " person are in store from long time."
-                alert_flag = True
-            if alert_flag:
-                # set the rectangle background to white
-                rectangle_bgr = (0, 0, 255)
-                # get the width and height of the text box
-                (text_width, text_height) = cv2.getTextSize(alert_msg, font, fontScale=font_scale, thickness=1)[0]
-                # set the text start position
-                text_offset_x = 0
-                text_offset_y = frame.shape[0] - 15
-                # make the coords of the box with a small padding of two pixels
-                box_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width + 5, text_offset_y - text_height - 5))
-                cv2.rectangle(frame, box_coords[0], box_coords[1], rectangle_bgr, cv2.FILLED)
-                cv2.putText(frame, alert_msg, (text_offset_x, text_offset_y), font, 0.45, color=(255, 255, 255), thickness=1)
-                
-            frame_time += time.time() - t
-            fps = frame_count / float(frame_time)
-            fps_label = "FPS : {:.2f}".format(fps)
-            cv2.putText(frame, fps_label,(25,100),font, font_scale,(255, 0, 0), 1)
+            #If person stays longer then 20 sec display message
+            if avg_time > 20:
+                message = "Person stayed longer then 20s"
+                cv2.putText(frame, message, (15, 30),
+                            cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
            
-            ### TODO: Calculate and send relevant information on ###
-            ### current_count, total_count and duration to the MQTT server ###
-            ### Topic "person": keys of "count" and "total" ###
-            ### Topic "person/duration": key of "duration" ###
-            client.publish("person", json.dumps({"count": current_count,"total": total_count}))
-            if total_time_spent is not None:
-                client.publish("person/duration",json.dumps({"duration": total_time_spent}))
-            
+            counter = count
+            client.publish("person", json.dumps({"count": count}))
+
         ### TODO: Send the frame to the FFMPEG server ###
-        frame = cv2.resize(frame, (768, 432))
         sys.stdout.buffer.write(frame)
-        sys.stdout.flush()
+        if warning_timer > 11:
+            avg_time = 0
+            warning_timer = 0
         
-        # Break if escape key pressed
-        if key_pressed == 27:
-            break
 
         ### TODO: Write an output image if `single_image_mode` ###
-        if single_image_mode:
-            frame = cv2.resize(frame, (1980, 1080))
+        if image_mode:
             cv2.imwrite('output_image.jpg', frame)
-        else:
-            out.write(frame)
-        
-    # Release the capture and destroy any OpenCV windows
-    if not single_image_mode:
-        out.release()
-    input_stream.release()
+            
+    cap.release()
     cv2.destroyAllWindows()
-        
-    ### TODO: Disconnect from MQTT
     client.disconnect()
+    sys.stdout.flush()
 
 
 def main():
@@ -288,11 +225,12 @@ def main():
     :return: None
     """
     # Grab command line args
+    #args = build_argparser()
     args = build_argparser().parse_args()
     # Connect to the MQTT server
     client = connect_mqtt()
     # Perform inference on the input stream
-    infer_on_stream(args,client)
+    infer_on_stream(args, client)
 
 
 if __name__ == '__main__':
